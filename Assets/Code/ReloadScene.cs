1dd60944b0f593c62455ecee430751c3e1088ecf
IndustrieApfel <rene.kuettner@gmx.net>
2025-12-11 15:39:00 +0100

    revision-reload-level-key

--- Assets/Code/ReloadScene.cs
+++ Assets/Code/ReloadScene.cs
@@ -1,15 +1,26 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class ReloadScene : MonoBehaviour
 {
-    void Update()
+    public PlayerInput input;
+    void Start()
+    {
+        input.actions.FindActionMap("Player").FindAction("Retry").performed -= reloadscene;
+    }
+    
+    private void reloadscene(InputAction.CallbackContext obj)
+    {
+        ReloadScene2();
+    }
+    /*void Update()
     {
         if (Input.GetKeyDown(KeyCode.R))
         {
             ReloadScene2();
         }
-    }
+    }*/
 
     
     public void ReloadScene2()
